a2a2924f0f0dc913eedf63e4a034a1b440cdc1f9
atasen19 <atasen19@itu.edu.tr>
2022-11-04 15:05:01 +0300

    wip

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 # Each resource type is implemented by a provider, which is a plugin for Terraform that offers a collection of resource types.
 provider "aws" {
-  region = "us-west-2"
+  region = var.aws_region
 }
 
 # A resource block declares a resource of a given type ("aws_instance") with a given local name ("web"). 
@@ -24,4 +24,68 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = var.instance_name
   }
+}
+
+# Data sources allow Terraform to use information defined outside of Terraform, 
+# defined by another separate Terraform configuration, or modified by functions.
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Modules are containers for multiple resources that are used together. 
+# A module consists of a collection of .tf and/or .tf.json files kept together in a directory.
+# Modules are the main way to package and reuse resource configurations with Terraform.
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  cidr = var.vpc_cidr_block
+  azs = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    project = "my-test-project",
+    environment = "dev"
+  }
+}
+
+module "app_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  name = "web-sg-my-test-project"
+  description = "Security group for web servers with HTTP ports open within VPC"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+
+  tags = {
+    project = "my-test-project",
+    environment = "dev"
+  }
+}
+
+module "lb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  name = "lb-sg-my-test-project"
+  description = "Security group for load balancer with HTTP ports open within VPC"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+
+  tags = {
+    project = "my-test-project",
+    environment = "dev"
+  }
+}
+
+resource "random_string" "lb_id" {
+  length  = 3
+  special = false
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
+# For more variable types please visit [here](https://developer.hashicorp.com/terraform/language/expressions/types)
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
   default     = "ExampleAppServerInstance"
+}
+
+variable "aws_region" {
+  description = "AWS Region"  
+  type = string
+  default = "eu-central-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "ingress_cidr_blocks" {
+  description = "Ingress CIDR block for Load Balancer Security Group"
+  type = list 
+  default = ["0.0.0.0/0"]
 }
