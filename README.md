52aa0037670416430278121c6ec3c39b790bd067
atasen19 <atasen19@itu.edu.tr>
2022-11-07 15:46:49 +0300

    wip

--- README.md
+++ README.md
@@ -39,7 +39,59 @@ This repository includes AWS deployment by using [Terraform](https://www.terrafo
 
 ## Understanding Terraform `init`, `fmt`, `validate`, `plan`, and `apply`
 
-`init`
-It's the inception of all the providers, plugins, and modules that your Terraform code needs to create resources.
-And you won't be able to deploy anything without executing this command first.
-It initializes your Terraform project's working folder, and downloads the required plugins from the appropriate registry or repository. As other stages of Terraform deployment require provider, plugins, and modules, this command needs to be run first!
+`terraform init`
+- It's the inception of all the providers, plugins, and modules that your Terraform code needs to create resources.
+- And you won't be able to deploy anything without executing this command first.
+- It initializes your Terraform project's working folder, and downloads the required plugins from the appropriate registry or repository. 
+- As other stages of Terraform deployment require provider, plugins, and modules, this command needs to be run first! 
+- And it is safe to run this command, as it doesn't modify, delete, or update any Terraform code. Though it does update plugins' providers which your Terraform code might be using.
+
+`terraform fmt`
+- A command to organize and beautify your Terraform template is Terraform format.
+- As the name implies, it's going to format your code to keep it consistent by checking the formatting against compliance standards.
+- And it is a good thing to run if your teams are working on the same project and pushing to version control systems, such as Git.
+- This command is also safe to run at any stage of the project.
+- It will only arrange your Terraform code in place, but will not modify the actual code.
+
+`terraform validate`
+- As the name implies, Terraform validate checks your Terraform code
+for syntax mistakes and internal consistency, for example, typos and misconfigured resources, where a parameter might be wrong in a given resource.
+- This command **depends on** Terraform init being run
+at least once before it can execute. Otherwise, it'll give an error. And also, this command is safe to run at any time, as it only makes recommendations, warnings, and outputs errors in your Terraform code.
+
+`terraform plan`
+- This is the command you'll find yourself executing a lot.
+- Terraform plan creates the plan of action for Terraform to act on.
+- It tests connectivity to provider APIs using the credentials that you provide.
+- It also refreshes the state of the resources.
+- It does this by calculating the delta between the current and the desired state, which is defined in the Terraform code.
+- This is kind of a fail-safe before you actually execute the code
+to create real resources. So use it often.
+- And finally, the execution plan can be saved to a file
+using the `-out` flag.
+- However, it will not hide any sensitive information
+being passed in the code, and will store that in plain text.
+- This execution plan can be directly passed to a Terraform apply command, as it helps save time, so that it doesn't go ahead and create the state of the resources defined in the code every single time.
+
+`terraform apply` (Deploy)
+- It applies the changes as suggested by the execution plan.
+And by default, it will prompt you one last time before creating real resources, for which you'll need to type "Yes" explicitly,
+or change the behavior by passing an optional flag so that you're not prompted.
+- By default, Terraform apply will show the current execution strategy if it is not being passed a file containing the execution plan, which shows all the modifications.
+- That is, creation, deletion, and update of resources that is needed to achieve the required state defined in the code.
+
+
+## Terraform Backends
+Terraform backends basically determine how the state is stored.
+You can either store it locally or remotely in a solution such as AWS S3. By default, the state is stored on your local disk. However, that behavior can be changed. It can be changed via passing the backend configuration to a Terraform block in your Terraform project code.
+However, one thing to note is that variables **cannot** be used
+as input to the Terraform block.
+Now this takes away from the flexibility of being able to interpolate variables inside this block, however this is a much requested feature and HashiCorp might enable this in the future.
+
+Now lets create an AWS S3 bucket from the terminal under project folder with the following command: 
+``` 
+aws s3api create-bucket --bucket a_unique_bucket_name --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+``` 
+*Note: The bucket name must be unique and you can change your region.*
+
+Now copy the name you just gave the bucket and put it to bucket feature in the file `backend.tf`.
--- main.tf
+++ main.tf
@@ -1,144 +1,144 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.16"
+#     }
+#   }
 
-  required_version = ">= 1.2.0"
-}
+#   required_version = ">= 1.2.0"
+# }
 
-# Each resource type is implemented by a provider, which is a plugin for Terraform that offers a collection of resource types.
-provider "aws" {
-  region = var.aws_region
-}
+# # Each resource type is implemented by a provider, which is a plugin for Terraform that offers a collection of resource types.
+# provider "aws" {
+#   region = var.aws_region
+# }
 
-# A resource block declares a resource of a given type ("aws_instance") with a given local name ("web"). 
-# The name is used to refer to this resource from elsewhere in the same Terraform module, but has no significance outside that module's scope.
-# The resource type and name together serve as an identifier for a given resource and so must be unique within a module.
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+# # A resource block declares a resource of a given type ("aws_instance") with a given local name ("web"). 
+# # The name is used to refer to this resource from elsewhere in the same Terraform module, but has no significance outside that module's scope.
+# # The resource type and name together serve as an identifier for a given resource and so must be unique within a module.
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.micro"
 
-  tags = {
-    name = var.instance_name
-  }
-}
+#   tags = {
+#     name = var.instance_name
+#   }
+# }
 
-# Data sources allow Terraform to use information defined outside of Terraform, 
-# defined by another separate Terraform configuration, or modified by functions.
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# # Data sources allow Terraform to use information defined outside of Terraform, 
+# # defined by another separate Terraform configuration, or modified by functions.
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
-# Modules are containers for multiple resources that are used together. 
-# A module consists of a collection of .tf and/or .tf.json files kept together in a directory.
-# Modules are the main way to package and reuse resource configurations with Terraform.
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+# # Modules are containers for multiple resources that are used together. 
+# # A module consists of a collection of .tf and/or .tf.json files kept together in a directory.
+# # Modules are the main way to package and reuse resource configurations with Terraform.
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
+#   version = "3.18.1"
 
-  cidr = var.vpc_cidr_block
-  azs = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
-  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
+#   cidr = var.vpc_cidr_block
+#   azs = data.aws_availability_zones.available.names
+#   private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+#   public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = var.enable_vpn_gateway
+#   enable_nat_gateway = true
+#   enable_vpn_gateway = var.enable_vpn_gateway
 
-  tags = var.resource_tags
-}
+#   tags = var.resource_tags
+# }
 
-# For detailed information please visit: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest/submodules/web
-module "app_security_group" {
-  source = "terraform-aws-modules/security-group/aws/modules/web"
-  version = "4.16.0"
+# # For detailed information please visit: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest/submodules/web
+# module "app_security_group" {
+#   source = "terraform-aws-modules/security-group/aws/modules/web"
+#   version = "4.16.0"
 
-  name = "web-sg-my-test-project"
-  description = "Security group for web servers with HTTP ports open within VPC"
-  vpc_id = module.vpc.vpc_id
+#   name = "web-sg-my-test-project"
+#   description = "Security group for web servers with HTTP ports open within VPC"
+#   vpc_id = module.vpc.vpc_id
 
-  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+#   ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
 
-  tags = var.resource_tags
-}
+#   tags = var.resource_tags
+# }
 
-# For detailed information please visit: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest/submodules/web
-module "lb_security_group" {
-  source  = "terraform-aws-modules/security-group/aws/modules/web"
-  version = "4.16.0"
+# # For detailed information please visit: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest/submodules/web
+# module "lb_security_group" {
+#   source  = "terraform-aws-modules/security-group/aws/modules/web"
+#   version = "4.16.0"
 
-  name = "lb-sg-my-test-project"
-  description = "Security group for load balancer with HTTP ports open within VPC"
-  vpc_id = module.vpc.vpc_id
+#   name = "lb-sg-my-test-project"
+#   description = "Security group for load balancer with HTTP ports open within VPC"
+#   vpc_id = module.vpc.vpc_id
 
-  ingress_cidr_blocks = var.ingress_cidr_blocks
+#   ingress_cidr_blocks = var.ingress_cidr_blocks
 
-  tags = var.resource_tags
-}
+#   tags = var.resource_tags
+# }
 
-resource "random_string" "lb_id" {
-  length  = 3
-  special = false
-}
+# resource "random_string" "lb_id" {
+#   length  = 3
+#   special = false
+# }
 
-# For detailed inputs and outputs please visit: https://registry.terraform.io/modules/terraform-aws-modules/elb/aws/latest
-module "elb_http" {
-  source = "terraform-aws-modules/elb/aws"
-  version = "3.0.1"
+# # For detailed inputs and outputs please visit: https://registry.terraform.io/modules/terraform-aws-modules/elb/aws/latest
+# module "elb_http" {
+#   source = "terraform-aws-modules/elb/aws"
+#   version = "3.0.1"
 
-  # Ensure lb name is unique
-  name = "lb-${random_string.lb_id.result}-my-test-project-dev"
+#   # Ensure lb name is unique
+#   name = "lb-${random_string.lb_id.result}-my-test-project-dev"
 
-  internal = false
+#   internal = false
 
-  # For detailed  output parameters please visit: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest/submodules/web#outputs
-  security_groups = [module.lb_security_group.this.security_group_id]
-  subnets = module.vpc.public_subnets
+#   # For detailed  output parameters please visit: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest/submodules/web#outputs
+#   security_groups = [module.lb_security_group.this.security_group_id]
+#   subnets = module.vpc.public_subnets
 
-  number_of_instances = length(module.ec2_instances.instance_ids)
-  instances = module.ec2_instances.instance_ids
+#   number_of_instances = length(module.ec2_instances.instance_ids)
+#   instances = module.ec2_instances.instance_ids
 
-  listener = [{
-    instance_port = "80"
-    instance_protocol = "HTTP"
-    lb_port = "80"
-    lb_protocol = "HTTP"
-  }]
+#   listener = [{
+#     instance_port = "80"
+#     instance_protocol = "HTTP"
+#     lb_port = "80"
+#     lb_protocol = "HTTP"
+#   }]
 
-  health_check = {
-    # The instance being checked. The protocol is either TCP, HTTP, HTTPS, or SSL. 
-    # The range of valid ports is one (1) through 65535.
-    target = "HTTP:80/index.html"
+#   health_check = {
+#     # The instance being checked. The protocol is either TCP, HTTP, HTTPS, or SSL. 
+#     # The range of valid ports is one (1) through 65535.
+#     target = "HTTP:80/index.html"
     
-    # The approximate interval, in seconds, between health checks of an individual instance. 
-    # Type: Integer. Valid Range: Minimum value of 5. Maximum value of 300.
-    interval = 10
+#     # The approximate interval, in seconds, between health checks of an individual instance. 
+#     # Type: Integer. Valid Range: Minimum value of 5. Maximum value of 300.
+#     interval = 10
 
-    # The number of consecutive health checks successes required before moving the instance to the Healthy state.
-    # Valid Range: Minimum value of 2. Maximum value of 10.
-    healthy_threshold = 3
+#     # The number of consecutive health checks successes required before moving the instance to the Healthy state.
+#     # Valid Range: Minimum value of 2. Maximum value of 10.
+#     healthy_threshold = 3
 
-    # The number of consecutive health check failures required before moving the instance to the Unhealthy state.
-    # Valid Range: Minimum value of 2. Maximum value of 10.
-    unhealthy_threshold = 10
+#     # The number of consecutive health check failures required before moving the instance to the Unhealthy state.
+#     # Valid Range: Minimum value of 2. Maximum value of 10.
+#     unhealthy_threshold = 10
 
-    # The amount of time, in seconds, during which no response means a failed health check.
-    # Valid Range: Minimum value of 2. Maximum value of 60.
-    timeout = 5
-  }
+#     # The amount of time, in seconds, during which no response means a failed health check.
+#     # Valid Range: Minimum value of 2. Maximum value of 60.
+#     timeout = 5
+#   }
 
-  tags = var.resource_tags
-}
+#   tags = var.resource_tags
+# }
 
-module "ec2_instances" {
-  source = "./modules/aws-instance"
+# module "ec2_instances" {
+#   source = "./modules/aws-instance"
 
-  instance_count     = 2
-  instance_type      = "t2.micro"
-  subnet_ids         = module.vpc.private_subnets[*]
-  security_group_ids = [module.app_security_group.this_security_group_id]
+#   instance_count     = 2
+#   instance_type      = "t2.micro"
+#   subnet_ids         = module.vpc.private_subnets[*]
+#   security_group_ids = [module.app_security_group.this_security_group_id]
 
-  tags = var.resource_tags
-}
+#   tags = var.resource_tags
+# }
